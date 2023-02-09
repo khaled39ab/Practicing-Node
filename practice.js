0561e73160556ec2db4e83726b9590b6509ac191
khaled39ab <khaled39ab@gmail.com>
2023-02-09 22:41:53 +0600

    successfully connect with frontend

--- practice.js
+++ practice.js
@@ -5,10 +5,18 @@ const app = express();
 
 app.use(cors())
 
-
+const products =[
+    {id: 1, name: 'pen', price: 10},
+    {id: 2, name: 'pencil', price: 15},
+    {id: 3, name: 'cutter', price: 20},
+]
 app.get('/', (req, res) =>{
     res.send('App is running')
 });
+
+app.get('/products', (req, res) =>{
+    res.send(products)
+})
 
 app.listen(port, () =>{
     console.log('App listening at', port);
