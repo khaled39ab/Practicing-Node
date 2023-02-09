d06094a9bdd6f36b0d3f224b2325791ce6bd31bd
khaled39ab <khaled39ab@gmail.com>
2023-02-09 23:32:12 +0600

    finnaly post api from frontend

--- practice.js
+++ practice.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const port = process.env.PORT || 4000;
 const app = express();
+const port = process.env.PORT || 4000;
 
-app.use(cors())
+app.use(cors());
+app.use(express.json())
 
 const products =[
     {id: 1, name: 'pen', price: 10},
@@ -16,7 +17,15 @@ app.get('/', (req, res) =>{
 
 app.get('/products', (req, res) =>{
     res.send(products)
-})
+});
+
+app.post('/products', (req,res)=>{
+    const product = req.body;
+    product.id = products.length + 1;
+    products.push(product)
+    console.log(product);
+    res.send(product)
+});
 
 app.listen(port, () =>{
     console.log('App listening at', port);
