63bbba820a2190108b05de18326b8800ff08639c
khaled39ab <khaled39ab@gmail.com>
2023-02-11 14:12:07 +0600

    a little bugs suffer me 2hours

--- practice.js
+++ practice.js
@@ -31,9 +31,9 @@ const run = async () => {
 
         app.get('/products/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             // console.log(id, query);
-            const result = productsCollection.findOne(query);
+            const result = await productsCollection.findOne(query);
             res.send(result);
         });
 
@@ -43,9 +43,9 @@ const run = async () => {
             res.send(result)
         });
 
-        app.delete('/products/:id', async(req, res) => {
+        app.delete('/products/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await productsCollection.deleteOne(query)
             res.send(result)
         });
